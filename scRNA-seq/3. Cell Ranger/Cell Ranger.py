15952919560eaa4d19e7bf3106833eb54d4b10da
Si Lin Yip <silinyip@gmail.com>
2023-08-16 09:54:07 +0200

    Update Cell Ranger

--- scRNA-seq/3. Cell Ranger/Cell Ranger.py
+++ scRNA-seq/3. Cell Ranger/Cell Ranger.py
@@ -46,9 +46,53 @@ def cellranger_count(file_path):
 cellranger_count("/media/usb/SILIN/PRJNA779978/")
 
 
+
 #cellranger count --id={OUTPUT_SAMPLE_NAME} \ #creates an output directory that is named using this id
 #                 --transcriptome={DIRECTORY_WITH_REFERENCE} \ #path to the Cell Ranger-compatible transcriptome reference
 #                 --fastqs={DIRECTORY_WITH_FASTQ_FILES} \ #path to the directory containing the FASTQ files
 #                 --sample={NAME_OF_SAMPLE_IN_FASTQ_FILES} \ #specify which samples to use - works off of the sample id at the beginning of the FASTQ file name
 #                 --localcores={NUMBER_OF_CPUS} \
 #                 --localmem={RAM_MEMORY}
+
+
+
+import os
+import pandas as pd
+
+df = pd.read_csv("/home/gh527/Downloads/filereport_read_run_PRJNA779978_tsv.txt", delimiter="\t") #read the CSV file into a DataFrame
+print(df.columns)
+print(df.head()) #display columns and preview the first few rows
+
+sample_names_list = list(df["run_accession"]) #extract sample names from the DataFrame
+samples_list = sample_names_list[0:]
+
+directory = "/media/usb/SILIN/yard/run_cellranger_count" #set the directory path
+
+try:
+    os.system("mkdir " + directory) #create the directory if it doesn't exist
+except:
+    pass
+
+os.system("cd " + directory) #change the working directory to the target directory
+
+def cellranger_count(file_path): #function to perform cellranger count analysis
+    dir_list = os.listdir(file_path)
+    print(dir_list)
+
+    for sample in dir_list:  #iterate through samples
+        dir_subf = os.listdir(os.path.join(file_path, sample))
+        print(dir_subf)
+
+        for fastq_file in dir_subf: #iterate through fastq files in each sample
+            fastq_path = os.path.join("/media/usb/SILIN/yard/run_cellranger_count", sample, fastq_file)
+            print(fastq_path)
+
+            lbl = sample
+            reference = "/media/usb/SILIN/yard/run_cellranger_count/" + reference_name  #set reference variable
+
+            cmd = "cellranger count --id=" + lbl + " --transcriptome=" + reference + " --fastqs=" + fastq_path + " --sample=" + lbl #command for cellranger count
+            print(cmd)
+
+            os.system(cmd) #execute the cellranger count command
+
+cellranger_count("/media/usb/SILIN/PRJNA779978/") #call the cellranger_count function on your specified path
