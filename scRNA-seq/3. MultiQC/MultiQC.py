331747acdb9386b4209ba0c0802c3177b0fab450
Si Lin Yip <138315630+silinyip@users.noreply.github.com>
2023-10-19 15:12:43 +0200

    Delete scRNA-seq directory

--- scRNA-seq/3. MultiQC/MultiQC.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def multiqc(directory_path):
-    outdir = "C:\\Users\\HP\\Downloads\\FastQC"
-    cmd = ("multiqc " + directory_path + " -o " + outdir)
-    os.system(cmd)
-
-multiqc("C:\\Users\\HP\\Downloads\\FastQC")
